129aa00b40c34eafb6b4f6e20d544202a2b6f4b8
Laura Gavilán Fernández <lauragavilan3@gmail.com>
2023-04-09 12:29:12 +0200

    InitCardsValue
    Añadida la función para inicializar las cartas de la baraja

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -31,9 +31,50 @@ public class Deck : MonoBehaviour
     {
         /*TODO:
          * Asignar un valor a cada una de las 52 cartas del atributo "values".
-         * En principio, la posición de cada valor se deberá corresponder con la posición de faces. 
-         * Por ejemplo, si en faces[1] hay un 2 de corazones, en values[1] debería haber un 2.
+         * En principio, la posicion de cada valor se debera corresponder con la posicion de faces. 
+         * Por ejemplo, si en faces[1] hay un 2 de corazones, en values[1] deberia haber un 2.
          */
+
+        
+        // El bucle recorre cada una de las cartas de la lista values[] que hemos inicializado antes
+        for(int i = 0; i < values.Length; i++)
+        {
+            // Le da el valor 11 -> (A) -> a la primera carta
+            if (cardIndex==0)
+            {
+                values[i] = 11;
+                // Sumamos una unidad al indice de las cartas
+                cardIndex += 1;
+            }
+            // A las siguientes 9 cartas -> de la 2 a la 10 -> le asinamos el valor que le corresponde
+            // Al dos de corazones -> valor 2
+            // Al tres de corazones -> valor 3...
+            else if (cardIndex <10)
+            {
+                values[i] = cardIndex + 1;
+                // Vamos sumando una unidad al indice para ir guardando el valor
+                cardIndex += 1;
+            }
+            // Una vez hemos dado valor a las 10 primeras cartas incluyendo las (A) -> le damos valor a J, Q, K
+            else if(cardIndex<13)
+            {
+                // A las cartas J, Q, K -> le añadimos el valor 10
+                values[i] = 10;
+                // El siguiente if comprueba si hemos completado toda la lista de carta del mismo palo
+                // Si ya le hemos dado valor a J, K y Q
+                if (cardIndex >= 12) // Es >= 12 porque las posiciones son siempre una menos del numero -> la lista empieza en [0], no es 1
+                {
+                    // el indice de la carta vuelve a ser 0 para pasar a dar valores al siguiente palo
+                    cardIndex = 0;
+                }
+                else
+                {
+                    // Si todavia no le hemos dado valor a las 13 cartas del palo, seguimos sumando una unidad al indice hasta hacerlo
+                    cardIndex += 1;
+                }
+            }
+        }
+        // El bucle termina cuando le hemos dado valor a las 52 cartas
     }
 
     private void ShuffleCards()
